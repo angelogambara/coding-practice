22cf459618db02aaf87f17f76f1239707ce491c6
Angelo Gambara <angelo@angelogambara.dev>
2026-04-19 23:06:23 +0200

    chore: one more assembly trick

--- src/cpp/helloworld-inline.c
+++ src/cpp/helloworld-inline.c
@@ -4,18 +4,28 @@ int main(int argc, char **argv) {
   char *msg = "Hello, World!\n";
   long msg_len = 14;
 
-  asm volatile (
-    ".intel_syntax noprefix;"
-    "mov rax, 1;"
-    "mov rdi, 1;"
-    "mov rsi, %0;"
-    "mov rdx, %1;"
-    "syscall;"
-    ".att_syntax prefix;"
-    :
-    : "r"(msg), "r"(msg_len)
-    : "rax", "rdi", "rsi", "rdx", "rcx", "r11", "memory"
-  );
+  asm volatile(".intel_syntax noprefix;"
+               /* 1. Print the contents of msg ("Hello, World!"). */
+               "mov rax, 1;"
+               "mov rdi, 1;"
+               "mov rsi, %0;"
+               "mov rdx, %1;"
+               "syscall;"
+               /* 2. Print using a symbol from inline. */
+               "mov rax, 1;"
+               "mov rdi, 1;"
+               "lea rsi, [rip + .Linline];"
+               "lea rdx, [.Lend - .Linline];"
+               "syscall;"
+               /* 3. Workaround to write data section. */
+               "jmp .Lend;"
+               ".Linline:"
+               ".ascii \"INLINE\\n\";"
+               ".Lend:"
+               ".att_syntax prefix;"
+               :
+               : "r"(msg), "r"(msg_len)
+               : "rax", "rdi", "rsi", "rdx", "rcx", "r11", "memory");
 
   return 0;
 }
